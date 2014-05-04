6330f414bc4aa062ee2d50620a941aaad9645a5f
kaizi99 <kaizi99@gmx.de>
2014-05-04 22:00:01 +0200

    Ban und Tempban Commands vorbereitet

---
+++ SCSP/src/de/scspteam/scsp/Ban.java
@@ -0,0 +1,14 @@
+package de.scspteam.scsp;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Ban 
+{
+	public static boolean onCommand(CommandSender sender, Command cmd, String label, String[] args, JavaPlugin plugin)
+	{
+		
+		return false;
+	}
+}
