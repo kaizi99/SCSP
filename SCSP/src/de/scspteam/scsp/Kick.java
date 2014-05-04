299c5c760fc8e02e6a4073538b67f730913f3d25
kaizi99 <kaizi99@gmx.de>
2014-05-04 21:37:11 +0200

    Kick-Command eingeführt, wurde jedoch noch nicht in plugin.yml und SCSP.jar eingefügt.

---
+++ SCSP/src/de/scspteam/scsp/Kick.java
@@ -0,0 +1,20 @@
+package de.scspteam.scsp;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Kick 
+{
+	public static boolean onCommand(CommandSender sender, Command cmd, String label, String[] args, JavaPlugin plugin)
+	{
+		if(args.length == 1)
+		{
+			Player ziel = plugin.getServer().getPlayer(args[0]);
+			ziel.kickPlayer("Sie wurden von diesem Server gekickt!");
+			return true;
+		}
+		return false;
+	}
+}
