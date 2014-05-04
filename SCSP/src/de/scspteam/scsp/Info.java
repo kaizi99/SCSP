78ad3d5a07ef89246b147683c3507c9e9dc59234
kaizi99 <LPinDE@outlook.com>
2014-05-04 16:11:24 +0200

    Jeder Command hat jetzt seine eigene Klasse

---
+++ SCSP/src/de/scspteam/scsp/Info.java
@@ -0,0 +1,39 @@
+package de.scspteam.scsp;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Info 
+{
+	public Info(CommandSender sender, Command cmd, String label, String[] args)
+	{
+		onCommand(sender, cmd, label, args);
+	}
+	
+	public static boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{	
+		//Command Info: Gibt Version und Entwickler des Plugins heraus
+		if(cmd.getName().equalsIgnoreCase("info"))
+		{
+			//Wird der Befehl aus der Konsole gemacht?
+			if(!(sender instanceof Player))
+			{
+				//Konsole die Nachricht senden
+				System.out.println("Version: 0.1");
+				System.out.println("Entwickler: kaizi99 und Der_Josh");
+				return true;
+			}
+			
+			//Playerobjekt wird erstellt und dem Sender des Commands zugewiesen
+			Player player = (Player) sender;
+			
+			//Dem Spieler die Nachrichten senden
+			player.sendMessage("Version: " + ChatColor.BLUE + "0.1");
+			player.sendMessage("Entwickler: " + ChatColor.BLUE + "kaizi99 und Der_Josh");
+			return true;
+		}
+		return false;
+	}
+}
--- SCSP/src/de/scspteam/scsp/SCSP.java
+++ SCSP/src/de/scspteam/scsp/SCSP.java
@@ -24,25 +24,9 @@ public class SCSP extends JavaPlugin
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{	
-		//Command Info: Gibt Version und Entwickler des Plugins heraus
 		if(cmd.getName().equalsIgnoreCase("info"))
 		{
-			//Wird der Befehl aus der Konsole gemacht?
-			if(!(sender instanceof Player))
-			{
-				//Konsole die Nachricht senden
-				System.out.println("Version: 0.1");
-				System.out.println("Entwickler: kaizi99 und Der_Josh");
-				return true;
-			}
-			
-			//Playerobjekt wird erstellt und dem Sender des Commands zugewiesen
-			Player player = (Player) sender;
-			
-			//Dem Spieler die Nachrichten senden
-			player.sendMessage("Version: " + ChatColor.BLUE + "0.1");
-			player.sendMessage("Entwickler: " + ChatColor.BLUE + "kaizi99 und Der_Josh");
-			return true;
+			return Info.onCommand(sender, cmd, label, args);
 		}
 		return false;
 	}
