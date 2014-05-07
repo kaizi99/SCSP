7ef0411efbe3394d5620132bfac8ca4e582b859d
kaizi99 <LPinDE@outlook.com>
2014-05-07 20:41:58 +0200

    Revert "Sachen gefixt und GS Command geht jetzt"
    This reverts commit 0cbb3881bd48477e2e1b21cc75b09b03b09ca306.

--- SCSP/src/de/scspteam/scsp/Gs.java
+++ SCSP/src/de/scspteam/scsp/Gs.java
@@ -2,28 +2,28 @@ package de.scspteam.scsp;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.ChatColor;
 
 public class Gs
 {
-	public static boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
-		if(args.length != 0)
-		{
-			return false;
-		}
-		
-		Player player = (Player)sender;
-		
-		player.sendMessage(ChatColor.GREEN + "Hallo, "+player.getName());	
-		player.sendMessage(ChatColor.GOLD + "[Skate큦 Community Server] " + ChatColor.DARK_RED + "Diese kommandos kannst du nutzen: ");	
-		player.sendMessage(ChatColor.GOLD + "[Skate큦 Community Server] " +  ChatColor.DARK_RED + "/gs erklaerung" );	
-		player.sendMessage(ChatColor.GOLD + "[Skate큦 Community Server] " +  ChatColor.DARK_RED + "/gs ask" );
-		if(player.hasPermission("gs.allow"))
-		{
-			player.sendMessage(ChatColor.GOLD + "[Skate큦 Community Server] " +  ChatColor.DARK_RED + "/gs allow" );
-		}
-		return  true;
+		   //kommando "Gs"
+		if(cmd.getName().equalsIgnoreCase("gs")){
+			
+				
+			sender.sendMessage(ChatColor.GREEN + "Hallo, "+sender.getName());	
+			sender.sendMessage(ChatColor.GREEN + "[Skate큦 Community Server] " + ChatColor.DARK_RED + "Diese kommandos kannst du nutzen: ");	
+			sender.sendMessage(ChatColor.BLUE + "[Skate큦 Community Server] " +  ChatColor.DARK_RED + "/gs erklaerung" );	
+			sender.sendMessage(ChatColor.BLUE + "[Skate큦 Community Server] " +  ChatColor.DARK_RED + "/gs ask" );
+			if(sender.hasPermission("gs.allow"))
+			{
+				sender.sendMessage(ChatColor.BLUE + "[Skate큦 Community Server] " +  ChatColor.DARK_RED + "/gs allow" );
+			}
+			return true;	
+			} else {
+				sender.sendMessage(ChatColor.BLUE + "[Skate큦 Community Server] " +  ChatColor.DARK_RED + "funktioniert." );
+				return  true;
+			}
 	}
 }
--- SCSP/src/de/scspteam/scsp/SCSP.java
+++ SCSP/src/de/scspteam/scsp/SCSP.java
@@ -46,10 +46,6 @@ public class SCSP extends JavaPlugin
 		{
 			return Ban.onCommand(sender, cmd, label, args, this);
 		}
-		if(cmd.getName().equalsIgnoreCase("gs"))
-		{
-			return Gs.onCommand(sender, cmd, label, args);
-		}
 		return false;
 	}
 }
