0b810deb93704bb852b6bb85b1ce228777064e8f
kaizi99 <LPinDE@outlook.com>
2014-05-10 21:52:25 +0200

    WorldEdit hinzugefügt, für Areas undso

--- SCSP/src/de/scspteam/scsp/gs/Gs_Main.java
+++ SCSP/src/de/scspteam/scsp/gs/Gs_Main.java
@@ -1,43 +1,19 @@
 package de.scspteam.scsp.gs;
 
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 public class Gs_Main 
 {
 	Player p;
-	Vector untenVorneRechts;
-	Vector untenVorneLinks;
-	Vector untenHintenRechts;
-	Vector untenHintenLinks;
-	
-	Vector obenVorneRechts;
-	Vector obenVorneLinks;
-	Vector obenHintenRechts;
-	Vector obenHintenLinks;
 	public Gs_Main(Player p)
 	{
 		this.p = p;
 	}
 	
-	
-	//Berechnungen:
-	//untenVorneLinks = 2 2 1
-	//untenHintenRechts = 1 1 2
-	//untenHintenLinks = 2 1 2
-	//obenVorneRechts = 1 2 1
-	//obenVorneLinks = 2 2 1
-	//obenHintenRechts = 1 2 2
-	public boolean bereichAuswaehlen(Vector pos1, Vector pos2)
+	public void glasSetzen()
 	{
-		untenVorneRechts = pos1;
-		obenHintenLinks = pos2;
-		untenVorneLinks = new Vector(pos2.getBlockX(), pos2.getBlockY(), pos1.getBlockZ());
-		untenHintenRechts = new Vector(pos1.getBlockX(), pos1.getBlockY(), pos2.getBlockZ());
-		untenHintenLinks = new Vector(pos2.getBlockX(), pos1.getBlockY(), pos2.getBlockZ());
-		obenHintenRechts = new Vector(pos1.getBlockX(), pos1.getBlockY(), pos2.getBlockZ());
-		obenVorneLinks = new Vector(pos2.getBlockX(), pos1.getBlockY(), pos1.getBlockZ());
-		obenVorneRechts = new Vector(pos1.getBlockX(), pos2.getBlockY(), pos1.getBlockZ());
-		return false;
+		
 	}
 }
